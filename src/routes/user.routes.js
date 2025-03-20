20766d3f822518af20fa0fd61de4c5aea8553d1f
soyvillareal <sebastiangarces152@gmail.com>
2025-03-20 06:12:21 -0500

    Actualizar el controlador de usuarios para obtener datos de la base de datos y habilitar la creación de usuarios

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { GetUsers } from '../controllers/user.controller.js';
+import { GetUsers, CreateUser } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get('/get', GetUsers);
-// router.post("/create", CreateUser)
+router.post("/create", CreateUser)
 
 export default router;
