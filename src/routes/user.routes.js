d1f16b15f52e054fa5aba9b8cbe99f145c9a7997
soyvillareal <sebastiangarces152@gmail.com>
2025-03-20 06:29:37 -0500

    Agregar funcionalidad para obtener datos de IP y la hora local; incluir axios como dependencia

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { GetUsers, CreateUser } from '../controllers/user.controller.js';
+import { GetUsers, CreateUser, GetDataIP } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get('/get', GetUsers);
+router.get("/ip", GetDataIP)
 router.post("/create", CreateUser)
 
 export default router;
