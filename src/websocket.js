830fd9bab6853d7c752bfedb149ecec372e172ed
soyvillareal <sebastiangarces152@gmail.com>
2025-03-21 04:45:55 -0500

    Mejorar la gestión de conexiones WebSocket; actualizar el formato de mensajes y manejar errores en la recepción de datos

--- src/websocket.js
+++ src/websocket.js
@@ -1,29 +1,36 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from "ws";
 
 export const setupWebSocket = (server) => {
     const wss = new WebSocketServer({ server });
 
-    wss.on('connection', (ws) => {
-        console.log('Cliente conectado');
+    wss.on("connection", (ws) => {
+        console.log("Cliente conectado");
 
         wss.clients.forEach(client => {
-            if (client !== ws && client.readyState === ws.OPEN) {
-                client.send(JSON.stringify({ type: "new_connection", message: "Un nuevo usuario se ha conectado" }));
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                const data = { message: "Un nuevo usuario se ha conectado" }
+                client.send(JSON.stringify({ type: "USER_CONNECTED", payload: data }));
+            }
+        });
+        
+        ws.on("message", (message) => {
+            console.log(`Mensaje recibido: ${message}`);
+
+            try {
+                const data = JSON.parse(message.toString());
+
+                wss.clients.forEach(client => {
+                    if (client !== ws && client.readyState === WebSocket.OPEN) {
+                        client.send(JSON.stringify(data));
+                    }
+                });
+            } catch (error) {
+                console.error("Error al procesar el mensaje:", error);
             }
         });
 
-        ws.on('message', (message) => {
-            console.log(`Mensaje recibido: ${message}`);
-
-            wss.clients.forEach(client => {
-                if (client.readyState === ws.OPEN) {
-                    client.send(`Servidor: ${message}`);
-                }
-            });
-        });
-
-        ws.on('close', () => {
-            console.log('Cliente desconectado');
+        ws.on("close", () => {
+            console.log("Cliente desconectado");
         });
     });
 
